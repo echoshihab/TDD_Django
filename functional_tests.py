5606a5a8ee956b7d524bce8d94b674afed9197a3
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2019-07-24 17:34:13 -0400

    add helper function for functional test

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_start_list_retrieve_list(self):
         # user goes to homepage to add todo item
         self.browser.get('http://localhost:8000')
@@ -33,27 +38,29 @@ class NewVisitorTest(unittest.TestCase):
         # user types 'submit patient payments to finance dept"
         inputbox.send_keys('Submit patient payments to finance dept')
 
-        # user hits ente, the page updates and the page lists "submit patient
+        # user hits enter, the page updates and the page lists "submit patient
         # payments to finance dept"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table(
+            '1: Submit patient payments to finance dept')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Submit patient payments to finance dept' for row in rows),
-            "New to-do item did not appear in table"
-        )
-
-        # user uses the text box to add another ites - "enter cashlog data via
+        # user uses the text box to add another ites - "Enter cashbox data via
         # cashlog form"
-        self.fail('Finish the test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Enter cashbox data via cashlog form')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # the page updates again, and now shows both item on the list
+        self.check_for_row_in_list_table(
+            '1: Submit patient payments to finance dept')
+        self.check_for_row_in_list_table(
+            '2: Enter cashbox data via cashlog form')
 
         # user wants the site to remember the list, and notes that the site has
         # generated a unique url
-
+        self.fail('Finish the test')
         # user visits the unique url to confirm that to do list is still
         # available
 
