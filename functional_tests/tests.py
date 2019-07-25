3f0e2a6f3aaf70fd468b877b1872f3b71b2e0b39
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2019-07-25 17:36:39 -0400

    extend funcitonal test to multiple users

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
+import unittest
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -28,7 +29,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_start_list_retrieve_list(self):
+    def test_can_start_list_for_one_user(self):
         # user goes to homepage to add todo item
         self.browser.get(self.live_server_url)
 
@@ -68,8 +69,52 @@ class NewVisitorTest(LiveServerTestCase):
 
         # user wants the site to remember the list, and notes that the site has
         # generated a unique url
-        self.fail('Finish the test')
+
         # user visits the unique url to confirm that to do list is still
         # available
 
         # user quites the todo list for the day
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+
+        #user1 starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Submit patient payments to finance dept')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table(
+            '1: Submit patient payments to finance dept')
+
+        # user1 notices that her list has a unique url
+        user1_list_url = self.browser.current_url
+        self.assertRegex(user1_list_url, '/lists/.+')
+
+        # now a new usr, user2, comes along to the site
+
+        # we use a new browser session to make sure no information is coming
+        # form user1
+        self.browser.quit()
+        self.browser = webdriver.Chrome(
+            "C:\\ChromeDriver\\chromedriver.exe")
+
+        # user2 visits the home page, there is no sign of user1's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Submit patient payments to finance dept', page_text)
+        self.assertNotIn('Enter a cashbox data via cashlog form', page_text)
+
+        # user2 starts a new list by entering a new item
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Clean Frontdesk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Clean Frontdesk')
+
+        # user2 gets his own unique URL
+        user2_list_url = self.browser.current_url
+        self.assertRegex(user2_list_url, '/lists/.+')
+        self.assertNotEqual(user2_list_url, user1_list_url)
+
+        # again no trace of user1 list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Submit patient payments to finance dept', page_text)
+        self.assertIn('Clean Frontdesk', page_text)
