c0172661cdaa069c04226ce38ac6c99acc47f06d
Shihab Khan (ext 1112) <skhan@lxa.on.ca>
2019-07-31 17:30:52 -0400

    new url, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,6 +19,7 @@ from lists import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home_page, name='home')
-
+    path('', views.home_page, name='home'),
+    path('lists/the-only-list-in-the-world/',
+         views.view_list, name='view_list'),
 ]
